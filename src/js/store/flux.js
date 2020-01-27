9c9d7d5f27e414e53b6563dd6eee2547485cb91c
Lucas Dearaujo <lucasaraujo45@outlook.com>
2020-01-27 03:45:07 +0000

    fetch api and added functionality

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,9 +1,27 @@
-const getState = ({ getStore, setStore }) => {
+const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			//Your data structures, A.K.A Entities
+			contacts: [],
+			id: ""
 		},
 		actions: {
+			loadSomeApi: () => {
+				fetch("https://assets.breatheco.de/apis/fake/contact/")
+					.then(response => response.json())
+					.then(data => {
+						let { store } = this.state;
+						store.contacts = data;
+						this.setState({ store });
+						console.log(store.contacts);
+					});
+			},
+
+			addContact: contact => {
+				Flux.dispatchEvent("contacts", dataToSave);
+				this.addEvent("contacts", data => {
+					return data;
+				});
+			}
 			//(Arrow) Functions that update the Store
 			// Remember to use the scope: scope.state.store & scope.setState()
 		}
